1ad2d7bc5e805b80a53ed4ba70adba0dc30ff60a
user <user@es302n16>
2017-03-14 15:56:10 +0800

    新增建構子

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -5,10 +5,16 @@ package oo.abstraction;
     int cc;
     String name;
     String type;
+    int status;
+    
     public Car(String brand,int cc,String name,String type){
         this.brand=brand;
         this.cc=cc;
         this.name=name;
         this.type=type;
     }
+
+	public Car(String string, String string2, int i) {
+		// TODO Auto-generated constructor stub
+	}
 }
--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -1,7 +1,11 @@
 package oo.abstraction;
 
-class CarTester{
+public class CarTester{
     public static void main(String[] args){
-        //Car c1=new Car("Toyota");
+        Car c1=new Car("TOYOTA","ALTIS",1800);
+        Car c2=new Car("HONDA","ACCORD",2000);
+        Car c3=new Car("HONDA","ACCORD",2000);
+       
+        
     }
 }
