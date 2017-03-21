7e1310491d57151cdb364f99eb67ba6cfdfa08c6
user <user@es302n17>
2017-03-21 16:24:32 +0800

    作業

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -5,7 +5,30 @@ public class CarTester{
         Car c1=new Car("TOYOTA","ALTIS",1800);
         Car c2=new Car("HONDA","ACCORD",2000);
         Car c3=new Car("HONDA","ACCORD",2000);
-       
+        int[] n=new int[5];
+        n[0]=0;
+        n[1]=0;
+        n[2]=0;
+        n[3]=0;
+        System.out.println(n[2]);
         
+        Car[] cars = new Car[3];
+        System.out.println(cars[0]);
+     cars [0] =new Car("TOYOTA","ALTIS",1800);
+     cars [1] =new Car("HONDA","ACCORD",2000);
+     cars [2] =new Car("TOYOTA","WISH",2000);
+     System.out.println(cars[0].name);
+     for(int i=0;i<=9;i++){
+    	 System.out.println(i);
+     }
+     System.out.println(i);
+     
+     class AA{
+    	 
+     }
+     AA a=new AA();
     }
+  
+    
+    
 }
