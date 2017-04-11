11bec72d9da879dbe7dafede0704e4f969143b19
user <user@es302n17>
2017-04-11 16:31:18 +0800

    作業

---
+++ src/oo/stati/Student.java
@@ -0,0 +1,26 @@
+package oo.stati;
+
+public class Student {
+	int english;
+	int math;
+	int chinese;
+	static int pass=60;
+	public Student(int english,int math,int chinese){
+		super();
+		this.english =english;
+		this.math=math;
+		this.chinese=chinese;
+	}
+	public void print(){
+		System.out.println(getScore(english)+"\t"+getScore(math)+"\t"+getScore(chinese));
+	}
+	private String getScore(int score) {
+		if(score<pass){
+			return String.valueOf(score)+"*";
+		}else{
+			return String.valueOf(score);
+		}
+	
+	}
+
+}
---
+++ src/oo/stati/Tester.java
@@ -0,0 +1,13 @@
+package oo.stati;
+
+public class Tester {
+	
+	public static void main(String[] args){
+		Student stu1=new Student(58,98,66);
+		Student stu2=new Student(89,56,77);
+		stu1.pass=70;
+		stu1.print();
+		stu2.print();
+	}
+
+}
