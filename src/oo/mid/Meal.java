1a9a345a7a74cbee83fd34f3b7d55446be531c5e
user <user@es302n17>
2017-04-25 14:26:08 +0800

    期中平時

---
+++ src/oo/mid/Meal.java
@@ -0,0 +1,23 @@
+package oo.mid;
+
+import java.util.ArrayList;
+
+public class Meal {
+String address;
+String food;
+String numbers;
+
+
+public Meal(String address,String food,String phone){
+	this.address=address;
+	this.food=food;
+	this.numbers=numbers;
+}
+public void foods(){
+	ArrayList<String> f=new ArrayList();
+	f.add("薯條");
+	f.add("大麥克");
+	f.add("可樂");
+	f.add("冰炫風");
+}
+}
---
+++ src/oo/mid/Ticket.java
@@ -0,0 +1,39 @@
+package oo.mid;
+
+public class Ticket {
+int id;
+String name;
+float price;
+
+public Ticket(int id,String name,float price){
+	this.id=id;
+	this.name=name;
+	this.price=price;
+}
+
+
+public void setId(int id){
+	this.id=id;
+}
+
+public void getid(int id){
+	this.id=id;
+}
+
+public void setName(String name){
+	this.name=name;
+}
+
+public void getName(String name){
+	this.name=name;
+}
+
+public void getPrice(float price){
+	this.price=price;
+}
+
+public void setPrice(float price){
+	this.price=price;
+}
+
+}
